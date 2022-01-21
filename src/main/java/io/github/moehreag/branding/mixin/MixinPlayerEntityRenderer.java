d4416697c7e6558788788aef9ccfe3f3c5ab6129
moehreag <moehreag@gmail.com>
2022-01-21 17:21:44 +0100

    Fix Levelhead  (Osmium) issue by using ModifyArgs
    to not having to use ci.cancel() which breaks compatability

--- src/main/java/io/github/moehreag/branding/mixin/MixinPlayerEntityRenderer.java
+++ src/main/java/io/github/moehreag/branding/mixin/MixinPlayerEntityRenderer.java
@@ -2,35 +2,30 @@ package io.github.moehreag.branding.mixin;
 
 import io.github.moehreag.branding.Axolotlclient;
 import net.minecraft.client.network.AbstractClientPlayerEntity;
-import net.minecraft.client.render.VertexConsumerProvider;
-import net.minecraft.client.render.entity.EntityRendererFactory;
-import net.minecraft.client.render.entity.LivingEntityRenderer;
 import net.minecraft.client.render.entity.PlayerEntityRenderer;
-import net.minecraft.client.render.entity.model.EntityModel;
-import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.text.LiteralText;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.ModifyArgs;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
 @Mixin(PlayerEntityRenderer.class)
-public abstract class MixinPlayerEntityRenderer extends LivingEntityRenderer{
+public abstract class MixinPlayerEntityRenderer{
 
-	public MixinPlayerEntityRenderer(EntityRendererFactory.Context ctx, EntityModel<net.minecraft.entity.Entity> model, float shadowRadius) {
-		super(ctx, model, shadowRadius);
-	}
 
-	@Inject(method = "renderLabelIfPresent(Lnet/minecraft/client/network/AbstractClientPlayerEntity;Lnet/minecraft/text/Text;Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V",
-	at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/entity/LivingEntityRenderer;renderLabelIfPresent(Lnet/minecraft/entity/Entity;Lnet/minecraft/text/Text;Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V", ordinal = 1), cancellable = true)
-	public void addLabel(AbstractClientPlayerEntity abstractClientPlayerEntity, Text text, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, CallbackInfo ci){
+	@ModifyArgs(method = "renderLabelIfPresent(Lnet/minecraft/client/network/AbstractClientPlayerEntity;Lnet/minecraft/text/Text;Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V",
+	at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/entity/LivingEntityRenderer;renderLabelIfPresent(Lnet/minecraft/entity/Entity;Lnet/minecraft/text/Text;Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V",
+	ordinal = 1))
+	public void addBadge(Args args){
+
+		AbstractClientPlayerEntity player = args.get(0);
+
+		if(Axolotlclient.CONFIG.showBadge && Axolotlclient.isUsingClient(player.getUuid())){
 
 		if (Axolotlclient.CONFIG.showBadge && Axolotlclient.isUsingClient(abstractClientPlayerEntity.getUuid())) {
 			text = new LiteralText("✵ ").append(text);
-			super.renderLabelIfPresent(abstractClientPlayerEntity, text, matrixStack, vertexConsumerProvider, i);
-			matrixStack.pop();
-			ci.cancel();
+			args.set(1, new LiteralText("✵ ").append((Text) args.get(1)));
 		}
 	}
 }
