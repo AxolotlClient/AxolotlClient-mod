c02a5abed0e01aaeeff755ca85db74f71415f55d
moehreag <moehreag@gmail.com>
2022-02-14 08:57:21 +0100

    hmhm huhu

--- src/main/java/io/github/moehreag/axolotlclient/mixin/MixinChatHud.java
+++ src/main/java/io/github/moehreag/axolotlclient/mixin/MixinChatHud.java
@@ -4,7 +4,6 @@ import io.github.moehreag.axolotlclient.Axolotlclient;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.hud.ChatHud;
 import net.minecraft.text.LiteralText;
-import net.minecraft.text.Style;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -16,35 +15,53 @@ import java.util.Objects;
 public class MixinChatHud {
 
 	@ModifyArg(method = "addMessage(Lnet/minecraft/text/Text;I)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/ChatHud;addMessage(Lnet/minecraft/text/Text;IIZ)V"))
-	public Text editChat(Text message){
-		assert MinecraftClient.getInstance().player != null;
+	public Text editChat(Text message) {
 
-		LiteralText name = new LiteralText("");
+		if (Axolotlclient.CONFIG.NickHider.hideOwnName || Axolotlclient.CONFIG.NickHider.hideOtherNames) {
+			assert MinecraftClient.getInstance().player != null;
 
-		LiteralText editedMessage = new LiteralText("");
-		String sender;
+			LiteralText name = new LiteralText("");
 
-		if (message.getString().contains(">")){
-			sender = message.getString().split(">")[0].split("<")[1];
-		} else if (message.getString().contains(":")){
-			sender = message.getString().split(":")[0];
-			if (sender.contains(" "))return message;
-		} else {return message;}
+			LiteralText editedMessage = new LiteralText("");
+			String sender;
 
-		if (Objects.equals(sender, MinecraftClient.getInstance().player.getName().getString())){
-			name.append(Axolotlclient.CONFIG.NickHider.hideOwnName ? new LiteralText(Axolotlclient.CONFIG.NickHider.OwnName): MinecraftClient.getInstance().player.getName());
-		} else {
-			name.append(new LiteralText(Axolotlclient.CONFIG.NickHider.hideOtherNames ? Axolotlclient.CONFIG.NickHider.otherName: sender));
-		}
+			if (message.getString().contains(">")) {
+				sender = message.getString().split(">")[0].split("<")[1];
+			} else if (message.getString().contains(":")) {
+				sender = message.getString().split(":")[0];
+				if (sender.contains("]")) {
+					String[] send = sender.split("] ");
+					sender = send[send.length - 1];
+				}if (sender.contains("[NPC]"))return message;
+				if (sender.contains(" ")) return message;
+			} else {return message;}
 
-			String[] msg = message.getString().split(message.getString().split(">")[0].split("<")[1]);
-			for (String s: msg){
-				editedMessage.append(s);
-				if (Objects.equals(s, msg[0]) && Axolotlclient.CONFIG.badgeOptions.showChatBadge){editedMessage.append(Axolotlclient.badge + " ");}
-				if(!Objects.equals(s, msg[msg.length - 1])){editedMessage.append(name);}
-
+			if (Objects.equals(sender, MinecraftClient.getInstance().player.getName().getString())) {
+				name.append(Axolotlclient.CONFIG.NickHider.hideOwnName ? new LiteralText(Axolotlclient.CONFIG.NickHider.OwnName) : MinecraftClient.getInstance().player.getName());
+			} else {
+				name.append(new LiteralText(Axolotlclient.CONFIG.NickHider.hideOtherNames ? Axolotlclient.CONFIG.NickHider.otherName : sender));
 			}
 
-		return editedMessage.setStyle(Style.EMPTY.withFont(Axolotlclient.FONT));
+			String[] msg = message.getString().split(sender);
+			for (String s : msg) {
+				editedMessage.append(s);
+				if (Objects.equals(s, msg[0]) && Axolotlclient.CONFIG.badgeOptions.showChatBadge && Axolotlclient.onlinePlayers.contains(sender)) {
+					editedMessage.append(Axolotlclient.badge);
+				}
+				if (!Objects.equals(s, msg[msg.length - 1])) {
+					editedMessage.append(name);
+				}
+			}
+
+
+		if (!editedMessage.getString().split(":")[0].contains("<") && editedMessage.getString().split(":")[0].contains("] " ) && !editedMessage.getString().contains("[NPC]")){
+			editedMessage = new LiteralText(editedMessage.getString().split("] ")[1]);
+		}
+
+
+			return editedMessage.setStyle(message.getStyle().withFont(Axolotlclient.FONT));
+		}
+		return message;
 	}
+
 }
