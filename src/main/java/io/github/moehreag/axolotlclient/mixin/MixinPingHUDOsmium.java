89de72ec055ff893757aa045328fb2127a4c8480
moehreag <moehreag@gmail.com>
2022-03-28 18:41:42 +0200

    update to 1.18.2
    Disable Osmium mixin as long as it hasn't been updated yet

--- src/main/java/io/github/moehreag/axolotlclient/mixin/MixinPingHUDOsmium.java
+++ src/main/java/io/github/moehreag/axolotlclient/mixin/MixinPingHUDOsmium.java
@@ -1,16 +1,16 @@
 package io.github.moehreag.axolotlclient.mixin;
 
-import com.intro.client.render.drawables.PingDisplay;
+//import com.intro.client.render.drawables.PingDisplay;
 import io.github.moehreag.axolotlclient.util.Util;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 
-@Mixin(PingDisplay.class)
+//@Mixin(PingDisplay.class)
 public class MixinPingHUDOsmium {
 
-	@ModifyArg(method = "render", at=@At(value = "INVOKE", target = "Lcom/intro/client/render/drawables/PingDisplay;drawCenteredText(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/font/TextRenderer;Ljava/lang/String;III)V"), index = 2)
+	/*@ModifyArg(method = "render", at=@At(value = "INVOKE", target = "Lcom/intro/client/render/drawables/PingDisplay;drawCenteredText(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/font/TextRenderer;Ljava/lang/String;III)V"), index = 2)
 	public String realPing(String par3){
 		return Util.currentServerPing+" ms";
-	}
+	}*/
 }
