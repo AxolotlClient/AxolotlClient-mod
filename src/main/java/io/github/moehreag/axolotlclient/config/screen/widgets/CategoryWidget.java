b7d070e013aed1c236ebfba9f63cf292e35b5d72
moehreag <moehreag@gmail.com>
2022-04-14 18:09:56 +0200

    better smaller Button Texture

--- src/main/java/io/github/moehreag/axolotlclient/config/screen/widgets/CategoryWidget.java
+++ src/main/java/io/github/moehreag/axolotlclient/config/screen/widgets/CategoryWidget.java
@@ -8,6 +8,7 @@ import net.minecraft.util.Identifier;
 public class CategoryWidget extends ButtonWidget {
 
     private final Identifier BUTTON_TEXTURE = new Identifier("axolotlclient", "textures/gui/button1.png");
+    private final Identifier BUTTON2_TEXTURE = new Identifier("axolotlclient", "textures/gui/button2.png");
 
     public OptionCategory category;
 
@@ -19,6 +20,7 @@ public class CategoryWidget extends ButtonWidget {
     @Override
     public void render(MinecraftClient client, int mouseX, int mouseY) {
         client.getTextureManager().bindTexture(BUTTON_TEXTURE);
+        if(width==100)client.getTextureManager().bindTexture(BUTTON2_TEXTURE);
         drawTexture(x, y, 0, 0,width, height, width, height);
         drawCenteredString(client.textRenderer, category.getTranslatedName() +"...", x+(width/2), y+(height-8)/2, -1);
     }
--- src/main/java/io/github/moehreag/axolotlclient/config/screen/widgets/OptionWidget.java
+++ src/main/java/io/github/moehreag/axolotlclient/config/screen/widgets/OptionWidget.java
@@ -10,6 +10,7 @@ public class OptionWidget extends CustomWidget{
 
     private final Option option;
     private final Identifier BUTTON_TEXTURE = new Identifier("axolotlclient", "textures/gui/button1.png");
+    private final Identifier BUTTON2_TEXTURE = new Identifier("axolotlclient", "textures/gui/button2.png");
     private final Identifier DIALOG_TEXTURE= new Identifier("axolotlclient", "textures/gui/dialog.png");
 
     public OptionWidget(Option option, int row, int line, int height, CustomWidget.action action){
@@ -27,6 +28,7 @@ public class OptionWidget extends CustomWidget{
         this.hovered = mouseX >= this.x && mouseY >= this.y && mouseX < this.x + this.width && mouseY < this.y + this.height;
 
         client.getTextureManager().bindTexture(BUTTON_TEXTURE);
+        if(width==100)client.getTextureManager().bindTexture(BUTTON2_TEXTURE);
         drawTexture(x, y, 0, 0,width, height, width, height);
         drawCenteredString(this.client.textRenderer, option.getTranslatedName(), (int) (x+width/(float)2), y+5, -1);
 
@@ -93,12 +95,6 @@ public class OptionWidget extends CustomWidget{
                     "", DIALOG_TEXTURE, false, true, (IntegerOption) option);
         } else if(option.getType() == OptionType.COLOR){
             return new ColorSelectorWidget(x+width+2, y, (ColorOption)option);
-            /*return new CustomButtonWidget(5,
-                    x+width+2,
-                    y,
-                    75,
-                    20,
-                    "", DIALOG_TEXTURE, (ColorOption) option);*/
         } else if(option.getType() == OptionType.ENUM){
             return new EnumWidget(x+width+2, y, 75, 20, ((EnumOption)option).get());
         }
