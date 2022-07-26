c1e099a234f9ed7b5c7cea86f7cae0f83062b7b1
moehreag <moehreag@gmail.com>
2022-07-26 09:10:04 +0200

    no_require_discord

--- src/main/java/io/github/axolotlclient/config/screen/widgets/BooleanWidget.java
+++ src/main/java/io/github/axolotlclient/config/screen/widgets/BooleanWidget.java
@@ -49,7 +49,7 @@ public class BooleanWidget extends ButtonWidget {
     }
 
     private void renderSwitch(MatrixStack matrixStack){
-        int x = option.get() ? this.x + width - 9: this.x;
+        int x = option.get() ? this.x + width - width/5: this.x;
         this.drawTexture(matrixStack, x, this.y, 0, 66 + (hovered ? 20:0), 4, this.height/2);
         this.drawTexture(matrixStack, x, this.y + height/2, 0, 86 - height/2 + (hovered ? 20:0), 4, this.height/2);
         this.drawTexture(matrixStack, x + 4, this.y, 200 - 4, 66 + (hovered ? 20:0), 4, this.height);
