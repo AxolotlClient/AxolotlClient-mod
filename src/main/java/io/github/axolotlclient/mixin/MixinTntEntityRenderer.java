3e569c86f4e3603e1e22c80540892308295b6917
moehreag <moehreag@gmail.com>
2022-08-21 10:47:07 +0200

    TntTime
    (and some other things)

---
+++ src/main/java/io/github/axolotlclient/mixin/MixinTntEntityRenderer.java
@@ -0,0 +1,32 @@
+package io.github.axolotlclient.mixin;
+
+import io.github.axolotlclient.modules.tnttime.TntTime;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.entity.EntityRenderDispatcher;
+import net.minecraft.client.render.entity.EntityRenderer;
+import net.minecraft.client.render.entity.TntEntityRenderer;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.TntEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TntEntityRenderer.class)
+public abstract class MixinTntEntityRenderer extends EntityRenderer<TntEntity> {
+
+    protected MixinTntEntityRenderer(EntityRenderDispatcher entityRenderDispatcher) {
+        super(entityRenderDispatcher);
+    }
+
+    @Inject(method = "render(Lnet/minecraft/entity/TntEntity;FFLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V",
+        at = @At(value = "INVOKE",
+            target = "Lnet/minecraft/client/render/entity/EntityRenderer;render(Lnet/minecraft/entity/Entity;FFLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V"),
+        cancellable = true)
+    public void render(TntEntity entity, float yaw, float delta, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int light, CallbackInfo ci){
+        if(TntTime.Instance.enabled.get()) {
+            super.renderLabelIfPresent(entity, TntTime.Instance.getFuseTime(entity.getFuseTimer()), matrixStack, vertexConsumerProvider, light);
+            ci.cancel();
+        }
+    }
+}
