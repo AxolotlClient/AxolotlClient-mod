346cbe4142ec1eae90737592c20ab19899de6635
moehreag <moehreag@gmail.com>
2023-07-02 02:45:48 +0200

    updated mappings

--- 1.20/src/main/java/io/github/axolotlclient/modules/hud/gui/hud/KeystrokeHud.java
+++ 1.20/src/main/java/io/github/axolotlclient/modules/hud/gui/hud/KeystrokeHud.java
@@ -128,7 +128,7 @@ public class KeystrokeHud extends TextHudEntry {
 					(stroke.getFGColor().getAsInt() & 16579836) >> 2 | stroke.getFGColor().getAsInt() & -16777216));
 			}
 		}));
-		KeyBind.unpressAll();
+		KeyBind.resetAll();
 		KeyBind.updatePressedStates();
 
 		onMouseMovementOption(mouseMovement.get());
